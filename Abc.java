102e2b29acee011e156fec2038f4b80f62ea1c1f
RazaSadiqWsn <sadiq.raza@wissen.com>
2022-03-15 09:40:00 +0530

    changed Abc.java , added checkPrime

--- Abc.java
+++ Abc.java
@@ -5,5 +5,16 @@ class Abc
 		System.out.println("Helloworld");
 		for(int i=0;i<10'i++)
 			System.out.println("Hello Wissen");
+
+		System.out.println(checkPrime(13));
+	}
+	public static boolean checkPrime(int n)
+	{
+		for(int i=2;i<=n/2;i++)
+		{
+			if(n%i==2)
+				return false;
+		}
+		return true;
 	}
 }
